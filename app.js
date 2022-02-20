64c962507ef34918e615cae3f896a7407cb43914
Whitney Harden <wtharden320@gmail.com>
2022-02-19 19:33:37 -0500

    setting up server

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// middleware
+app.use(cors())
+app.use(express.json()); // allows us access to req.body
 
 //listen to port number
 app.listen(8888, () => {

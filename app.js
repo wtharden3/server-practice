578fdaa7bd76b20be26aa6db0d09e8d6c696a453
Whitney Harden <wtharden320@gmail.com>
2022-02-23 07:55:34 -0500

    get one todo by id finished and starting update todo

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(express.json()); // allows us access to req.body
 app.post('/createtodo', async (req, res) =>{
   try {
     const {description} = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING * ", [description])
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING * ", [description]) //this has to be in array
     res.json(newTodo.rows[0]);
-    console.log(`"${description}" inserted into todo table`);
+    console.log(`"${description}" was inserted into todo table`);
   } catch (err) {
     console.error(err.message);
   }
@@ -27,13 +27,31 @@ app.get('/alltodos', async (req,res) => {
     const alltodos = await pool.query("SELECT * FROM todo");
     res.json(alltodos.rows);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
   }
 })
 
 //get a todo
+app.get('/todo/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    console.log(id);
+    const atodo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id] ) //this as to be in array
+    res.send(atodo.rows[0])
+    console.log(`here is the todo ${atodo.rows[0].description}`);
+  } catch (err) {
+    console.error(`[ERROR] ${err.message}`);
+  }
+})
 
 //update a todo
+app.put('todo/:id', (req, res) => {
+  try {
+    const {id} = req.params; //destructuring
+  } catch (err) {
+    console.error(`[ERROR] ${err.message}`);
+  }
+})
 
 //delete a todo
 
